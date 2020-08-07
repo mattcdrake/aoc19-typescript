1f5706c3aff0afc3877e7fb6da70313c97d54454
Matt Drake <drake.mattc@gmail.com>
2020-08-06 23:55:13 -0400

    Finish Day 1 part 2

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
-import { part1 as d1p1 } from "./day1";
+import { part1 as d1p1, part2 as d1p2 } from "./day1";
 
 console.log(d1p1());
+console.log(d1p2());
--- day1.ts
+++ day1.ts
@@ -1,7 +1,22 @@
 const fs = require("fs");
 
+const data = fs.readFileSync("./input/day1.txt", "UTF-8");
+const lines = data.split(/\n/);
+lines.pop();
+const nums = lines.map((x: string) => Number(x));
+
 const getFuelReq: (mass: number) => number = function (mass) {
   return Math.floor(mass / 3) - 2;
+};
+
+// Finds fuel requirements recursively
+const getFuelReqRec: (mass: number) => number = function (mass) {
+  let temp = getFuelReq(mass);
+  if (temp <= 0) {
+    return 0;
+  } else {
+    return temp + getFuelReqRec(temp);
+  }
 };
 
 const createReducer: (
@@ -13,11 +28,11 @@ const createReducer: (
 };
 
 let part1: () => number = function () {
-  const data = fs.readFileSync("./input/day1.txt", "UTF-8");
-  const lines = data.split(/\n/);
-  lines.pop();
-  const nums = lines.map((x: string) => Number(x));
   return nums.reduce(createReducer(getFuelReq), 0);
 };
 
-export { part1 };
+let part2: () => number = function () {
+  return nums.reduce(createReducer(getFuelReqRec), 0);
+};
+
+export { part1, part2 };
