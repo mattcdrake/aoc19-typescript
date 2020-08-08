9e381ade24a73410b28ae9dcb70f7d7492a59d23
Matt Drake <drake.mattc@gmail.com>
2020-08-08 00:17:48 -0400

    Add tests for past days

---
+++ tests/tests.ts
@@ -0,0 +1,32 @@
+import { part1 as d1p1, part2 as d1p2 } from "../day1";
+import { part1 as d2p1, part2 as d2p2 } from "../day2";
+import { expect } from "chai";
+import "mocha";
+
+describe("Testing day 1, part 1", () => {
+  it("should equal 3514064", () => {
+    const result = d1p1();
+    expect(result).to.equal(3514064);
+  });
+});
+
+describe("Testing day 1, part 2", () => {
+  it("should equal 5268207", () => {
+    const result = d1p2();
+    expect(result).to.equal(5268207);
+  });
+});
+
+describe("Testing day 2, part 1", () => {
+  it("should equal 3760627", () => {
+    const result = d2p1();
+    expect(result).to.equal(3760627);
+  });
+});
+
+describe("Testing day 2, part 2", () => {
+  it("should equal 7195", () => {
+    const result = d2p2();
+    expect(result).to.equal(7195);
+  });
+});
